6db0b29ee52324aae922f3219a05e864bb2a2f60
Linton M <lintonmackereth@gmail.com>
2019-11-09 15:47:45 +0000

    test added to check a player starts with 5 lives, Hangmand constructor created with playerLives getter and setter.

--- Game/Game/Classes/Hangman.cs
+++ Game/Game/Classes/Hangman.cs
@@ -3,8 +3,13 @@ namespace GameClasses
 {
     public class Hangman
     {
+        //private int playerLives;
+        public int playerLives { get; set; }
+
         public Hangman()
         {
+            playerLives = 5;
+
         }
     }
 }
--- Game/GameTests/HangmanTest.cs
+++ Game/GameTests/HangmanTest.cs
@@ -5,11 +5,28 @@ namespace GameTests
 {
     public class HangmanTest
     {
+        // A game is started
+        // A user starts with 5 lives
+        // A word is chosen, which is the correct answer
+        // A user chooses a letter at random
+        // the users choice of letters should be logged
+        // the users lives shoud be tracked
+        // the user should be shown the correct guesses
+        // until the word is guessed or the user runs out of lives
+
         [Fact]
         public void InstatiateHangmanInstantiationTest()
         {
             GameClasses.Hangman hangman = new GameClasses.Hangman();
             Assert.IsType<GameClasses.Hangman>(hangman);
+        }
+
+        [Fact]
+        public void CheckTheUsersLivesStartAtFive()
+        {
+            GameClasses.Hangman hangman = new GameClasses.Hangman();
+            Assert.Equal(hangman.playerLives, 5);
+
         }
     }
 }
